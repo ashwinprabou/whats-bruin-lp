3b885883255de2160e4a9d40489fcd194223bca0
Ashwin Prabou <aprabou@ucsc.edu>
2025-04-27 05:18:21 -0700

    made folders, organized

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Problem from "./components/Problem";
+import Solution from "./components/Solution";
+import Features from "./components/Features";
+import Testimonials from "./components/Testimonials";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-out",
+      once: false,
+    });
+  }, []);
+
+  return (
+    <div className="font-sans bg-neutral-50">
+      <Navbar />
+      <main>
+        <Hero />
+        <Problem />
+        <Solution />
+        <Features />
+        <Testimonials />
+        <About />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
